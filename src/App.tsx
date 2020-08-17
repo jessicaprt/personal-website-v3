040ee3e4c17e8fb56b1ac01ee515ec1cf700ab74
Jessica Antonious <jessica@prieto.ca>
2020-08-17 19:29:42 -0400

    update package.json

--- src/App.tsx
+++ src/App.tsx
@@ -6,44 +6,45 @@ import { Experience } from './components/experience/Experience';
 import { Highlights } from './components/highlights/Highlights';
 import { CustomFooter } from './components/footer/CustomFooter';
 
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { AboutMe } from './components/about-me/AboutMe';
 import { PageInfo } from './components/page-info/PageInfo';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <div>
-        <Route exact={true} 
-          path='/'
-          render={() => (
-            <div className="App">
-              <Welcome/>
-              <Experience/>
-              <Highlights/>
-              <CustomFooter/>
-          </div>
-        )}/>
-
-        <Route exact={true} 
-          path='/about-me'
-          render={() => (
-            <div className="App">
-              <AboutMe/>
-              <CustomFooter/>
+        <Switch>
+          <Route exact={true} 
+            path='/'
+            render={() => (
+              <div className="App">
+                <Welcome/>
+                <Experience/>
+                <Highlights/>
+                <CustomFooter/>
             </div>
-        )}/>
+          )}/>
 
-        <Route exact={true} 
-          path='/page-info'
-          render={() => (
-            <div className="App">
-              <PageInfo/>
-              <CustomFooter/>
-            </div>
-        )}/>
+          <Route exact={true} 
+            path='/about-me'
+            render={() => (
+              <div className="App">
+                <AboutMe/>
+                <CustomFooter/>
+              </div>
+          )}/>
+
+          <Route exact={true} 
+            path='/page-info'
+            render={() => (
+              <div className="App">
+                <PageInfo/>
+                <CustomFooter/>
+              </div>
+          )}/>
+        </Switch>
       </div>
-      
     </BrowserRouter>
     
   );
